b845f227814a7e40265c8c04b987940496b71de2
Lucas Cheung <lucascheungls@gmail.com>
2019-02-25 16:52:58 +0100

    added seed bars

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,10 @@
+require 'csv'
+
 Swipe.destroy_all
 User.destroy_all
+Coupon.destroy_all
+Bar.destroy_all
+MeetUpTime.destroy_all
 Match.destroy_all
 
 puts "Creating Users"
@@ -12,25 +17,9 @@ carol = User.create(email: "carol@gmail.com", password: "123456")
 puts "Done with users"
 chicks = [mary, susan, sue, gillian, carol]
 
-puts 'Swipe'
-chicks.each do |chick|
-  Swipe.create(swiper: bob, swipee: chick, like: [true, false].sample)
-  Swipe.create(swiper: chick, swipee: bob, like: [true, false].sample)
-  puts "Created swipe"
+
+csv_options = { col_sep: ',', quote_char: '"', headers: :first_row }
+
+CSV.foreach('db/bars.csv', csv_options) do |row|
+  Bar.create!(name:row['name'], location:row['location'], description:row['description'])
 end
-
-puts 'bar'
-bar = Bar.create!(name:'dirty dicks', description:'yooooo', location:'london')
-
-puts 'coupon'
-coupon = Coupon.create!(used:false)
-
-puts 'meet up time'
-meet = MeetUpTime.create(first_user_accepted:false, last_user_accepted:false)
-
-
-
-puts 'match'
-Match.create!(first_user: bob, last_user:sue, bar:bar, coupon:coupon, meet_up_time:meet)
-puts 'done match'
-
