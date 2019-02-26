f452116216000c0ab8eaf8e102c15a8ab94a2805
Lucas Cheung <lucascheungls@gmail.com>
2019-02-26 17:32:37 +0100

    swipe-filter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
   def index
-    @users = User.all
+    @swiped_id = []
+    if current_user.swipes == []
+      @users = User.all
+    else
+      current_user.swipes.each { |swipe| @swiped_id << swipe.swipee.id }
+      @users = User.where("id NOT IN (?)", @swiped_id)
+    end
   end
 end
