e1596c4a147075bcf120b8a21515f88f7e3fd1c0
Lucas Cheung <lucascheungls@gmail.com>
2019-02-27 19:42:38 +0100

    done

--- app/javascript/packs/preferenceChange.js
+++ app/javascript/packs/preferenceChange.js
@@ -1,29 +1,29 @@
 const maleBtn = document.querySelector(".option-circle .male");
 const femaleBtn = document.querySelector(".option-circle .female");
 
-function createButtonListener(love) {
-  return function(event) {
-    var cards = document.querySelectorAll(".tinder--card:not(.removed)");
-    var moveOutWidth = document.body.clientWidth * 1.5;
+// function createButtonListener(love) {
+//   return function(event) {
+//     var cards = document.querySelectorAll(".tinder--card:not(.removed)");
+//     var moveOutWidth = document.body.clientWidth * 1.5;
 
-    if (!cards.length) return false;
+//     if (!cards.length) return false;
 
-    var card = cards[0];
+//     var card = cards[0];
 
-    card.classList.add("removed");
+//     card.classList.add("removed");
 
-    if (love) {
-      card.style.transform =
-        "translate(" + moveOutWidth + "px, -100px) rotate(-30deg)";
-        swipedRight(event.target.id);
-    } else {
-      card.style.transform =
-        "translate(-" + moveOutWidth + "px, -100px) rotate(30deg)";
-        swipedLeft(event.target.id);
-    }
+//     if (love) {
+//       card.style.transform =
+//         "translate(" + moveOutWidth + "px, -100px) rotate(-30deg)";
+//         swipedRight(event.target.id);
+//     } else {
+//       card.style.transform =
+//         "translate(-" + moveOutWidth + "px, -100px) rotate(30deg)";
+//         swipedLeft(event.target.id);
+//     }
 
-    initCards();
+//     initCards();
 
-    event.preventDefault();
-  };
-}
+//     event.preventDefault();
+//   };
+// }
