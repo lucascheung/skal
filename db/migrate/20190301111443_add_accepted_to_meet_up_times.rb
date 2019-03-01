f9bcfddfd1392a3e5d8a4cf146c9bb0b59511cee
Lucas Cheung <lucascheungls@gmail.com>
2019-03-01 16:17:33 +0100

    remove default nil

--- db/migrate/20190301111443_add_accepted_to_meet_up_times.rb
+++ db/migrate/20190301111443_add_accepted_to_meet_up_times.rb
@@ -1,6 +1,6 @@
 class AddAcceptedToMeetUpTimes < ActiveRecord::Migration[5.2]
   def change
-    change_column :meet_up_times, :first_user_accepted, default: nil
-    change_column :meet_up_times, :last_user_accepted, default: nil
+    change_column :meet_up_times, :first_user_accepted
+    change_column :meet_up_times, :last_user_accepted
   end
 end
