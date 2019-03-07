fd41cc925868d4baa34fe0dc76bf17e225148b20
Lucas Cheung <lucascheungls@gmail.com>
2019-03-07 12:50:41 +0100

    delete geocode

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,3 @@
-require 'pry-byebug'
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
